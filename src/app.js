d5b1e160ccf0f38bed048849039f552b4dae87a8
alwahid786 <wahidforjob@gmail.com>
2025-09-24 20:24:59 +0500

    psuh live code

--- src/app.js
+++ src/app.js
@@ -19,7 +19,12 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://apply-super-quick.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://apply-super-quick.vercel.app",
+      "https://apply-super-quick-client.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
 );
